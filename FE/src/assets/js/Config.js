5edec238dc527c582e5aaa5c454c6e0d2d8d73c6
Feng Guo <feng@gruden.com>
2019-01-10 23:35:51 +0800

    working on ui

--- FE/src/assets/js/Config.js
+++ FE/src/assets/js/Config.js
@@ -1,8 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 //const API_URL = "http://127.0.0.1:3000"
-const API_URL = "http://192.168.123.1:3000"
-//const API_URL = "http://192.168.31.33:3000"
+//const API_URL = "http://192.168.123.1:3000"
+const API_URL = "http://192.168.31.33:3000"
 
 
 const Themes = [
@@ -16,12 +16,14 @@ const Theme = Themes[1]
 //"amber" "orange accent-4"//"pink accent-2"//"amber" orange
 
 const ProjectStatus = [
-	{ value: 1, label: '筹备中' },
-	{ value: 2, label: '在建中' },
-	{ value: 3, label: '已竣工' },
-	{ value: 4, label: '已验收' },
-	{ value: 5, label: '已停工' },
+	{ value: 1, text: '筹备中' },
+	{ value: 2, text: '在建中' },
+	{ value: 3, text: '已竣工' },
+	{ value: 4, text: '已验收' },
+	{ value: 5, text: '已停工' },
 ]
+
+const ProjcetType = ['新建','扩建','改建','拆建','恢复','其他']
 
 const TaskStatus = []
 
@@ -29,5 +31,6 @@ export default {
 	API_URL,
 	Theme,
 	ProjectStatus,
-	TaskStatus
+	TaskStatus,
+	ProjcetType
 }
--- FE/src/components/project/Create.vue
+++ FE/src/components/project/Create.vue
@@ -4,10 +4,74 @@
             项目基础信息
         </v-stepper-step>
         <v-stepper-content step="1">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click="step++">下一步</v-btn>
+            <v-card color="grey lighten-5" class="mb-5">
+                <!-- 项目信息 -->
+                <v-form v-model="valid1" lazy-validation ref="createProjectForm1">
+                    <v-container>
+                        <v-layout>
+                            <v-flex xs6 md6>
+                                <v-text-field v-model="project.name" :counter="25" label="项目名称" :rules="nameRules" required append-icon="business"></v-text-field>
+                            </v-flex>
+                            <v-flex xs3 md3>
+                                <v-text-field v-model="project.no" :counter="10" mask="##########" label="项目编号"></v-text-field>
+                            </v-flex>
+                            <v-flex xs3 md3>
+                                <v-select v-model="project.type" :items="config.ProjcetType" label="项目类型"></v-select>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex xs6 md6>
+                                <v-text-field v-model="project.description" :counter="50" label="项目简介"></v-text-field>
+                            </v-flex>
+                            <v-flex xs3 md3>
+                                <v-text-field v-model="project.acreage" mask="##########" label="施工面积（㎡）"></v-text-field>
+                            </v-flex>
+                            <v-flex xs3 md3>
+                                <v-text-field v-model="project.cost" mask="#######" label="工程造价(万元)" append-icon="attach_money"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex xs12 md4>
+                                <v-text-field v-model="project.contractNo" label="施工合同编号"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-text-field v-model="project.warrantNo" label="施工许可证号"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-text-field v-model="project.backupNo" mask="##########" label="备案号"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex xs12 md4>
+                                <v-menu :close-on-content-click="false" v-model="dateMenu1" :nudge-right="40" lazy transition="scale-transition" offset-y full-width min-width="290px">
+                                    <v-text-field slot="activator" v-model="project.startDate" label="开工日期" prepend-icon="event" readonly></v-text-field>
+                                    <v-date-picker v-model="project.startDate" @input="dateMenu1 = false"></v-date-picker>
+                                </v-menu>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-menu :close-on-content-click="false" v-model="dateMenu2" :nudge-right="40" lazy transition="scale-transition" offset-y full-width min-width="290px">
+                                    <v-text-field slot="activator" v-model="project.endDate" label="竣工日期" prepend-icon="event" readonly></v-text-field>
+                                    <v-date-picker v-model="project.endDate" @input="dateMenu2 = false"></v-date-picker>
+                                </v-menu>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-select v-model="project.status" :items="config.ProjectStatus" label="项目状态"></v-select>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex xs12 md6>
+                                <v-text-field v-model="project.location" label="项目地点" prepend-icon="location_on"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 md6>
+                                <v-text-field v-model="project.firstParty" label="项目甲方"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-form>
+            </v-card>
+            <v-btn color="primary" @click="validate1" :disabled="!valid1">下一步</v-btn>
         </v-stepper-content>
-        <v-stepper-step :complete="step > 2" step="2">工程施工阶段</v-stepper-step>
+        <v-stepper-step :complete="step > 2" step="2">设置工程施工阶段</v-stepper-step>
         <v-stepper-content step="2">
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
             <v-btn color="primary" @click="step++">下一步</v-btn>
@@ -39,12 +103,29 @@ export default {
     },
     data() {
         return {
-            step:1,
+            step: 1,
+            valid1: true,
+            dateMenu1: false,
+            dateMenu2: false,
+            nameRules: [
+                v => !!v || '请输入项目名称',
+                v => (v && v.length <= 25) || '最多输入25个字符'
+            ],
+            project: {
+                startDate: new Date().toISOString().substr(0, 10),
+                endDate: new Date().toISOString().substr(0, 10),
+            }
         }
     },
     methods: {
-
-    },
+        validate1() {
+            if (this.$refs.createProjectForm1.validate()) {
+                //this.snackbar = true
+                this.step++
+                console.log(this.project);
+            }
+        },
+    }
 }
 
 </script>
