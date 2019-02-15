9ad4cedb8b9d8a756a399a8275e5dccb87131073
Feng Guo <feng@gruden.com>
2019-02-15 18:45:17 +0800

    changed UAT

--- FE/src/assets/js/Config.js
+++ FE/src/assets/js/Config.js
@@ -1,7 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 //const API_URL = "http://127.0.0.1:3000"
-const API_URL = "http://103.254.77.30:8001/API"
+//const API_URL = "http://103.254.77.30:8001/API"
+const API_URL = "http://114.115.178.72/FineWork/API"
 //const API_URL = "http://localhost:9909/API"
 //const API_URL = "/API"
 
